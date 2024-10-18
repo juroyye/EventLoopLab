7208bb8ddd95b8a67fc43586d2e8577482abca58
Justin M <manningjustin08@yahoo.com>
2024-10-18 16:16:24 -0400

    Completed Part 2

--- main.js
+++ main.js
@@ -1,18 +1,46 @@
-let count = 0
+// let count = 0
 
-try{
-    
+// try{
 
-function increment (){
-    count = count + 1
-    console.log(count)
-    increment()
+// function increment (){
+//     count = count + 1
+//     console.log(count)
+//     increment()
+// }
+
+// increment()
+// } catch(e){
+
+//     console.log(e)
+//     console.log(count)
+
+// }
+
+const nestedArr = [1, 2, [3, 4, 5, [6, [7]]]];
+
+function flatten(arr, depth) {
+  return function () {
+    if (depth > 0) {
+      return flatten(arr.flat(1), depth - 1);
+    } else {
+      return arr;
+    }
+  };
 }
 
-increment()
-} catch(e){
+console.log(flatten(nestedArr, 5))
 
-    console.log(e)
-    console.log(count)
+function trampoline(fn) {
+  let result = fn;
 
+  while (typeof result === "function") {
+    result = result();
+  }
+
+  return result;
 }
+
+console.log(trampoline(flatten(nestedArr, 5)));
+
+
+
