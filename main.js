ad48d5b29daf575d51123e8bc4d898bc19489373
Justin M <manningjustin08@yahoo.com>
2024-10-31 16:33:57 -0400

    uncommented code to allow it to run

--- main.js
+++ main.js
@@ -1,46 +1,46 @@
-// let count = 0
+let count = 0
 
-// try{
+try{
 
-// function increment (){
-//     count = count + 1
-//     console.log(count)
-//     increment()
-// }
+function increment (){
+    count = count + 1
+    console.log(count)
+    increment()
+}
 
-// increment()
-// } catch(e){
+increment()
+} catch(e){
 
-//     console.log(e)
-//     console.log(count)
+    console.log(e)
+    console.log(count)
 
-// }
+}
 
-// const nestedArr = [1, 2, [3, 4, 5, [6, [7]]]];
+const nestedArr = [1, 2, [3, 4, 5, [6, [7]]]];
 
-// function flatten(arr, depth) {
-//   return function () {
-//     if (depth > 0) {
-//       return flatten(arr.flat(1), depth - 1);
-//     } else {
-//       return arr;
-//     }
-//   };
-// }
+function flatten(arr, depth) {
+  return function () {
+    if (depth > 0) {
+      return flatten(arr.flat(1), depth - 1);
+    } else {
+      return arr;
+    }
+  };
+}
 
-// console.log(flatten(nestedArr, 5))
+console.log(flatten(nestedArr, 5))
 
-// function trampoline(fn) {
-//   let result = fn;
+function trampoline(fn) {
+  let result = fn;
 
-//   while (typeof result === "function") {
-//     result = result();
-//   }
+  while (typeof result === "function") {
+    result = result();
+  }
 
-//   return result;
-// }
+  return result;
+}
 
-// console.log(trampoline(flatten(nestedArr, 5)));
+console.log(trampoline(flatten(nestedArr, 5)));
 
 let header = document.querySelector("h1")
 
@@ -72,6 +72,7 @@ let newEl = document.createElement('h2')
 newEl.textContent = pushedArray
 header.appendChild(newEl)
 alert("Calculations are complete!")
+
 
 
 
